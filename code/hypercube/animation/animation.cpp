25d04673025a51b78ab64bae002c43b059a74b44
YXH_XianYu <2943003@qq.com>
2022-12-09 20:52:14 +0800

    Feat: Hypercube动画系统&Demo

--- code/hypercube/animation/animation.cpp
+++ code/hypercube/animation/animation.cpp
@@ -4,7 +4,7 @@
 
 namespace Hypercube {
 
-Animation::Animation(int type, int left_frames) : type_(type), total_frames_(left_frames), left_frames_(left_frames) {}
+Animation::Animation(int type, int left_frames) : type_(type), total_frames_(left_frames), left_frames_(left_frames), tag_(0) {}
 
 int Animation::type() const { return type_; }
 
@@ -15,5 +15,13 @@ int Animation::left_frames() const { return left_frames_; }
 void Animation::Update() {
     if (left_frames_ > 0) left_frames_--;
 }
+
+int Animation::tag() const { return tag_; }
+
+void Animation::set_tag(int tag) { tag_ = tag; }
+
+int Animation::tag_id() const { return tag_id_; }
+
+void Animation::set_tag_id(int tag_id) { tag_id_ = tag_id; }
 
 }  // namespace Hypercube
--- code/hypercube/animation/animation.h
+++ code/hypercube/animation/animation.h
@@ -14,10 +14,18 @@ class Animation {
     int left_frames() const;
     void Update();
 
+    int tag() const;
+    void set_tag(int tag);
+
+    int tag_id() const;
+    void set_tag_id(int tag_id);
+
    protected:
     int type_;
     int total_frames_;
     int left_frames_;
+    int tag_;     // for you, do anything you want
+    int tag_id_;  // for you, do anything you want
 };
 
 }  // namespace Hypercube
