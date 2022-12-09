271a1d416bb30352533f9302d2ce76e3b4aba10a
Brony01 <101876608+Brony01@users.noreply.github.com>
2022-12-09 23:20:47 +0800

    Update BGM.h

--- code/audio/BGM.h
+++ code/audio/BGM.h
@@ -1,24 +1,14 @@
 #ifndef BGM_H
 #define BGM_H
-#include <QMediaPlaylist>
-#include <QMediaPlayer>
-#include <QtMultimedia>
-#include <qmediaplaylist.h>
-#include <qmediaplayer.h>
-//播放背景音乐（初始化、切换、暂停、播放）
-//游戏内音效（初始化、触发）
-
+#include <QSound>
 
 class BGM{
 public:
-    //添加音频文件到播放列表里,dir是路径，比如qrc:/new/sound/sounds/di.wav,   recurrent是否循环播放
-    //返回player
-    QMediaPlayer* init(QString dir,bool recurrent);
-
-    QMediaPlayer* add(QString dir,QMediaPlayer* player);//增加播放列表中的曲目或音效
-    QMediaPlayer* remove(int location,QMediaPlayer* player);//按添加顺序的position移除播放列表的曲目
-    void play(QMediaPlayer *player);//播放
-    void pause(QMediaPlayer* player);//暂停
+    QSound *open=new QSound("../2022-BJTU-DataStructrue-FinalProject/resource/sounds/open_win.wav");
+    QSound *close=new QSound("../2022-BJTU-DataStructrue-FinalProject/resource/sounds/close_win.wav");
+    QSound *switch_task=new QSound("../2022-BJTU-DataStructrue-FinalProject/resource/sounds/switch_task.wav");
+    QSound *switch_type=new QSound("../2022-BJTU-DataStructrue-FinalProject/resource/sounds/switch_type.wav");
+    QSound *music=new QSound("../2022-BJTU-DataStructrue-FinalProject/resource/sounds/music.wav");
 private:
 
 };
