14e1833546ee1768d7d90bff845e145f5c2c07b0
FuLiqi <1832271620@qq.com>
2022-12-15 15:36:53 +0800

    美好recordwindow

--- code/window/recordrankwindow.cpp
+++ code/window/recordrankwindow.cpp
@@ -3,9 +3,10 @@
 #include "../database/rank.h"
 #include "ui_recordrankwindow.h"
 
-RecordRankWindow::RecordRankWindow(int score, QWidget *parent) : QWidget(parent), ui(new Ui::RecordRankWindow), score_(score) {
-    ui->setupUi(this);
-    setFocus();
+RecordRankWindow::RecordRankWindow(int score, QWidget *parent)
+    : QWidget(parent), ui(new Ui::RecordRankWindow), score_(score) {
+  ui->setupUi(this);
+  setFocus();
 }
 
 RecordRankWindow::~RecordRankWindow() { delete ui; }
@@ -13,13 +14,14 @@ RecordRankWindow::~RecordRankWindow() { delete ui; }
 void RecordRankWindow::on_skip_button_clicked() { this->close(); }
 
 void RecordRankWindow::on_record_button_clicked() {
-    Rank::GetInstance()->Insert(ui->lineEdit->text().toStdString(), score_);
-    this->close();
+  Rank::GetInstance()->Insert(ui->lineEdit->text().toStdString(), score_);
+  this->close();
 }
 
 int RecordRankWindow::score() const { return score_; }
 
 void RecordRankWindow::set_score(int score) {
-    score_ = score;
-    ui->label->setText(QString::fromStdString("获得了" + std::to_string(score) + "分！谱写下你的ID吧！"));
+  score_ = score;
+  ui->label->setText(QString::fromStdString("获得了" + std::to_string(score) +
+                                            "分！谱写下你的ID吧！"));
 }
